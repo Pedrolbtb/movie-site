12a9c873509e83b6e78019976b530388c1c6a6c5
Pedrolbtb <pedro.lbtb@gmail.com>
2025-06-21 11:53:06 -0300

    routes consertado

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,6 +1,10 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
+    {
+    path: 'movies/:id',
+    renderMode: RenderMode.Server 
+  },
   {
     path: '**',
     renderMode: RenderMode.Prerender
